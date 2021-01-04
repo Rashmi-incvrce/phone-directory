8e6b1506618f680ce10027c79917cddc490b90d1
Rashmi <rashmi.incvrce@gmail.com>
2021-01-04 13:47:58 +0530

    Created Skeleton of Home Page

--- src/App.js
+++ src/App.js
@@ -2,7 +2,16 @@
 function App() {
   return (
     <div>
+    <div>
       Phone Directory
+    </div>
+    <button>Add</button>
+    <label htmlFor="name">Name:</label>
+    <input id="name" defaultValue="Rashmi"/>
+    <div>
+       <span>Name</span>
+       <span>Phone</span>
+    </div>
     </div>
   );
 }
